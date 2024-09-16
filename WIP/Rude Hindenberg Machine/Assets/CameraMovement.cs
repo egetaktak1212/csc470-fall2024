7efb9c501530b6b3ade0b3eb6aafcede7a2e9b1b
egetaktak1212 <ebtaktak@gmail.com>
2024-09-15 20:45:29 -0400

    finished (explanation for some things in desc)
    i didnt use any poly pizza, made everything. the textures for the grass and dirt was just me messing around in photoshop. the seesaw is also from blender, it took too long to set up hit boxes, i hate it. the rope is from blender too, i rigged it over there.

--- WIP/Rude Hindenberg Machine/Assets/CameraMovement.cs
+++ WIP/Rude Hindenberg Machine/Assets/CameraMovement.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    float speed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,23 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.position += transform.up * speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            transform.position += -transform.up * speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += -transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += transform.right * speed;
+        }
+
+
+
     }
 
 }
