82011cd720c36a47fb8e096722866c2552c4393a
egetaktak1212 <ebtaktak@gmail.com>
2024-09-19 14:35:30 -0400

    plane forward
    transform.forward was way too slow but i fixed it

--- WIP/9-11-24/Assets/plane_script.cs
+++ WIP/9-11-24/Assets/plane_script.cs
@@ -11,11 +11,11 @@ public class plane_script : MonoBehaviour
 
     float xRotationSpeed = 90f;
     float yRotationSpeed = 90f;
-    public float forwardSpeed = 90f;
+    public float forwardSpeed = 0.2f;
     bool boost = false;
     float Timer;
     float time;
-
+    int booster = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,17 +25,19 @@ public class plane_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float vAxis = Input.GetAxis("Vertical");
-        float hAxis = Input.GetAxis("Horizontal");
+        float hAxis = Input.GetAxis("Horizontal"); // -1 if left is pressed, 1 if right is pressed, 0 if neither
+        float vAxis = Input.GetAxis("Vertical"); // -1 if down is pressed, 1 if up is pressed, 0 if neither
 
+        // Apply the rotation based on the inputs
         Vector3 amountToRotate = new Vector3(0, 0, 0);
         amountToRotate.x = vAxis * xRotationSpeed;
-        amountToRotate.x = vAxis * yRotationSpeed;
-        amountToRotate *= Time.deltaTime;
+        amountToRotate.y = hAxis * yRotationSpeed;
+        amountToRotate *= Time.deltaTime; // amountToRotate = amountToRotate * Time.deltaTime;
         transform.Rotate(amountToRotate, Space.Self);
 
-        transform.position += transform.forward * forwardSpeed * Time.deltaTime;
 
+        transform.position += transform.forward * Time.deltaTime *10 * booster;
+        //print(booster);
         Vector3 camPos = transform.position;
         camPos += -transform.forward * 10f;
         camPos += Vector3.up * 8f;
@@ -47,16 +49,18 @@ public class plane_script : MonoBehaviour
 
     }
 
-    public void OnCollisionEnter(GameObject other)
+    private void OnTriggerEnter(Collider other)
     {
+        print("collision");
         time = Timer;
-        if (other.CompareTag("Booster")) {
+        if (other.CompareTag("Booster"))
+        {
             Boost();
         }
     }
 
     public void Boost() {
-        forwardSpeed *= 2;
+        booster = 2;
     }
 
 
