a5e233bd579a5acd8ede0de08a8483c218b3dde7
egetaktak1212 <ebtaktak@gmail.com>
2024-09-27 00:02:13 -0400

    panic commit in case the 72 hours ends at midnight
    i'm struggling a lot with the terrain. unity doesnt let you add collision to it unless you have kinematic turned off, which ruins the plane tech. So, i have to make terrain in blender and export it to unity for collision, which i need time to do. time i don't have. besides that, i have collectibles. plane movement tech, cool stuff

--- WIP/9-11-24/Assets/Scripts/plane_script.cs
+++ WIP/9-11-24/Assets/Scripts/plane_script.cs
@@ -1,27 +1,36 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class plane_script : MonoBehaviour
 {
+
     public GameObject cameraObject;
     public GameObject rotationObjectY;
     public GameObject rotationObjectX;
     public GameObject plane;
+    public TMP_Text juiceMeter;
+
+
+    static int score = 0;
 
     float xRotationSpeed = 120f;
     float zRotationSpeed = 120f;
     float yRotationSpeed = 100f;
     float forwardSpeedMult;
-    float forwardSpeed = 20f;
-    bool boost = false;
+    float forwardSpeed = 40f;
+    static bool boost = false;
+    static float boostTimer = 0;
     float Timer = 0;
     float time;
-    int booster = 1;
+    static int booster = 1;
     float gradRotate;
     float gradRotateBig;
     float slowDown;
@@ -42,15 +51,42 @@ public class plane_script : MonoBehaviour
     float wingSpeedNoneA = 0;
     float wingSpeedPowerA = 0;
     float wingSpeedPowerD = 0;
+    float boostFOV = 0;
+    float superBoostFOV = 0;
+    static float superBoostTimer;
+    static bool superBoost;
+    float superBooster = 1;
+
+
+    private void OnLevelWasLoaded(int level)
+    {
+        superBoostTimer = 0;
+        superBoost = false;
+        booster = 1;
+        boostTimer = 0;
+        boost = false;
+        booster = 1;
+        score = 0;
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        turnDeltaCalc = turnAmount;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        juiceMeter.text = "JUICE METER: " + score;
+
+        if (Input.GetKey(KeyCode.R)) {
+            SceneManager.LoadScene(0);
+
+        }
+
+
         float hAxis = -Input.GetAxis("Horizontal"); // -1 if left is pressed, 1 if right is pressed, 0 if neither
         float vAxis = Input.GetAxis("Vertical"); // -1 if down is pressed, 1 if up is pressed, 0 if neither
         float yAxis = Input.GetAxis("Yaw");
@@ -143,7 +179,7 @@ public class plane_script : MonoBehaviour
                 { //if slowdownbool is 1, that means we were going right. this means that slowDown is going down
                     if (slowDown > 0)
                     {
-                        slowDown += -slowDownBool * 0.02f * Time.deltaTime * Mathf.Pow(2, slowPower);
+                        slowDown += -slowDownBool * 0.1f * Time.deltaTime * Mathf.Pow(2, slowPower);
                         slowPower += 1f * Time.deltaTime;
                     }
                     else
@@ -155,7 +191,7 @@ public class plane_script : MonoBehaviour
                 { //slowdownbool negative, we goin left, this means slowDown is goin up
                     if (slowDown < 0)
                     {
-                        slowDown += -slowDownBool * 0.01f * Time.deltaTime * Mathf.Pow(2, slowPower);
+                        slowDown += -slowDownBool * 0.1f * Time.deltaTime * Mathf.Pow(2, slowPower);
                         slowPower += 1f * Time.deltaTime;
                     }
                     else
@@ -236,7 +272,7 @@ public class plane_script : MonoBehaviour
         //END OF GRAD MOVEMENT CODE
 
         //Rotates usign its position as the axis. It's the same as having an empty parented
-        turnAmount = 2 * ((amountToRotate.y * gradRotate) + slowDown);
+        turnAmount = (2*(amountToRotate.y * gradRotate) + slowDown);
         transform.RotateAround(transform.position, Vector3.up, turnAmount);
         //find out max and min turn amount, turn it into an angle. max is 180, min is 0. turn based off that
 
@@ -263,8 +299,66 @@ public class plane_script : MonoBehaviour
 
         //plane is an empty that controls the plane mesh. so i just rotate the plane mesh without affecting the movement of the actual plane. the plane can rotate however it wants now
 
+        //boost mechanic
+        if (boost) {
+            if (boostTimer < 5)
+            {
+                booster = 4;
+                boostTimer += Time.deltaTime;
+                if (boostFOV < 20) {
+                    boostFOV += 50 * Time.deltaTime;
+                }
+            }
+            else if (boostTimer >= 5)
+            {
 
-        transform.position += transform.forward * Time.deltaTime * forwardSpeed * booster;
+
+                booster = 1;
+                boostTimer = 0;
+                boost = false;
+            }     
+        }
+        if (boostTimer == 0) {
+            if (boostFOV > 0)
+            {
+                boostFOV -= 20 * Time.deltaTime;
+
+            }
+        }
+
+        if (superBoost)
+        {
+            if (superBoostTimer < 20)
+            {
+                superBooster = 10;
+                superBoostTimer += Time.deltaTime;
+                if (superBoostFOV < 50)
+                {
+                    superBoostFOV += 120 * Time.deltaTime;
+                }
+            }
+            else if (superBoostTimer >= 20)
+            {
+
+
+                superBooster = 1;
+                superBoostTimer = 0;
+                superBoost = false;
+            }
+        }
+        if (superBoostTimer == 0)
+        {
+            if (superBoostFOV > 0)
+            {
+                superBoostFOV -= 120 * Time.deltaTime;
+
+            }
+        }
+
+
+
+
+        transform.position += transform.forward * Time.deltaTime * forwardSpeed * booster * superBooster;
 
         //code to make the wings turn when plane is turning
         if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
@@ -324,23 +418,25 @@ public class plane_script : MonoBehaviour
 
                 plane.transform.Rotate(0, 0, wingSpeedNoneA * 900 * Time.deltaTime, Space.Self);
                 wingSpeedNoneA -= .5f * Time.deltaTime * wingSpeedPowerA;
-                wingSpeedPowerA /= 1.1f;
+                wingSpeedPowerA /= 1.005f;
             }
             if (Rotation < -0.5)
             {
 
                 plane.transform.Rotate(0, 0, wingSpeedNoneD * 900 * Time.deltaTime, Space.Self);
                 wingSpeedNoneD += .5f * Time.deltaTime * wingSpeedPowerD;
-                wingSpeedPowerD /= 1.1f;
+                wingSpeedPowerD /= 1.005f;
             }
 
         }
+
+
 
         forwardSpeedMult = RotationX * 0.1f;
         forwardSpeed = 20f + forwardSpeedMult;
 
 
-        Camera.main.fieldOfView = 60 + forwardSpeed * 1f;
+        Camera.main.fieldOfView = 60 + forwardSpeed * 1f + boostFOV + superBoostFOV;
 
         //cam controls
         Vector3 camPos = transform.position;
@@ -349,32 +445,50 @@ public class plane_script : MonoBehaviour
         cameraObject.transform.position = camPos;
         Vector3 thing = new Vector3(0, 1, 0);
         cameraObject.transform.LookAt(transform.position + thing);
-
-
+        
 
 
         Timer += Time.deltaTime;
         //Debug.Log(wingSpeedA + "    " + wingSpeedD + "     "+ Rotation + "         " + turnAmount);
-        Debug.Log(booster);
+        //Debug.Log(boostTimer + "   "  + boostFOV);
 
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
-        
-        time = Timer;
+    
         if (other.CompareTag("Booster"))
         {
+            boostTimer = 0;
+            boost = true;
 
-            this.booster = 2;
+        }
+
+        if (other.CompareTag("SuperBoost")) {
+            superBoostTimer = 0;
+            superBoost = true;
+        
+        }
+
+        if (other.CompareTag("Collectible")) { 
+            Destroy(other.gameObject);
+            score++;
+            
+        }
+
+        
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Terrain")) {
+            Debug.Log("collided");
+            SceneManager.LoadScene("SampleScene");
         }
     }
 
-    //public void Boost()
-    //{
-        
-    //    booster = 2;
-    //}
+
 
 
 }
